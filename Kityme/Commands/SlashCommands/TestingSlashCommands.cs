eeba8c808e1de102f7ad467918fed9be6f742550
igorunderplayer <igorschug6@gmail.com>
2021-09-24 16:58:51 -0300

    fix

--- Kityme/Commands/SlashCommands/TestingSlashCommands.cs
+++ Kityme/Commands/SlashCommands/TestingSlashCommands.cs
@@ -1,9 +1,5 @@
 ﻿using DSharpPlus;
 using DSharpPlus.SlashCommands;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Kityme.Commands
