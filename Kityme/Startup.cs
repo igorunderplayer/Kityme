924bb41fd8a94cd7fcee103f2e634685ff7e2d65
igorunderplayer <igorschug6@gmail.com>
2022-02-11 23:02:00 -0300

    Update Startup.cs

--- Kityme/Startup.cs
+++ Kityme/Startup.cs
@@ -16,91 +16,89 @@ using DSharpPlus.Net;
 using DSharpPlus.SlashCommands;
 using Kityme.Commands;
 using Kityme.Commands.SlashCommands;
-using Kityme.Entities;
 using Kityme.Events.Client;
-using Kityme.Extensions;
 using Kityme.Managers;
 using Kityme.Utils;
-using Newtonsoft.Json;
 
 namespace Kityme
 {
-    public class Startup
+  public class Startup
+  {
+    public DiscordClient Client { get; private set; }
+    public CommandsNextExtension Commands { get; private set; }
+    public SlashCommandsExtension SlashCommands { get; private set; }
+    public LavalinkExtension Lavalink { get; private set; }
+    private BotConfig botConfig;
+
+    public async Task RunAsync()
     {
-        public DiscordClient Client { get; private set; }
-        public CommandsNextExtension Commands { get; private set; }
-        public SlashCommandsExtension SlashCommands { get; private set; }
-        public LavalinkExtension Lavalink { get; private set; }
-        private BotConfig botConfig;
+      string root = Directory.GetCurrentDirectory();
+      string dotenv = Path.Combine(root, ".env");
+      DotEnv.Load(dotenv);
 
-        public async Task RunAsync()
-        {
-            string root = Directory.GetCurrentDirectory();
-            string dotenv = Path.Combine(root, ".env");
-            DotEnv.Load(dotenv);
+      botConfig = new BotConfig(
+          Environment.GetEnvironmentVariable("TOKEN"),
+          Environment.GetEnvironmentVariable("LAVALINK_HOST"),
+          Environment.GetEnvironmentVariable("SLAVALINK_HOST"),
+          Environment.GetEnvironmentVariable("LAVALINK_PASSWORD"),
+          Environment.GetEnvironmentVariable("MONGO_URL")
+      );
 
-            botConfig = new BotConfig(
-                Environment.GetEnvironmentVariable("TOKEN"),
-                Environment.GetEnvironmentVariable("LAVALINK_HOST"),
-                Environment.GetEnvironmentVariable("SLAVALINK_HOST"),
-                Environment.GetEnvironmentVariable("LAVALINK_PASSWORD"),
-                Environment.GetEnvironmentVariable("MONGO_URL")
-            );
+      var config = new DiscordConfiguration
+      {
+        Token = botConfig.Token,
+        TokenType = TokenType.Bot,
+        AutoReconnect = true,
+        UseRelativeRatelimit = true,
 
-            var config = new DiscordConfiguration
-            {
-                Token = botConfig.Token,
-                TokenType = TokenType.Bot,
-                AutoReconnect = true,
-                UseRelativeRatelimit = true,
-
-                Intents = DiscordIntents.Guilds
-                          | DiscordIntents.GuildMessages
-                          | DiscordIntents.GuildVoiceStates
-            };
+        Intents = DiscordIntents.Guilds
+                    | DiscordIntents.GuildMessages
+                    | DiscordIntents.GuildVoiceStates
+      };
 
 
-            Client = new DiscordClient(config);
+      Client = new DiscordClient(config);
 
-            Client.Ready += new Ready(Client).Client_Ready;
-            Client.VoiceStateUpdated += new VoiceStateUpdated(Client).Client_VoiceStateUpdated;
-            Client.ComponentInteractionCreated += Client_ComponentInteractionCreated;
-            Client.MessageCreated += async (DiscordClient client, MessageCreateEventArgs e) =>
-            {
-                if (e.Message.Content.StartsWith($"<@{client.CurrentUser.Id}>") ||
-                    e.Message.Content.StartsWith($"<!@{client.CurrentUser.Id}>"))
-                    await e.Message.RespondAsync("'- se quise ve meus comando usa k!help 👍");
-                
-                return;
-            };
+      Client.Ready += new Ready(Client).Client_Ready;
+      Client.VoiceStateUpdated += new VoiceStateUpdated(Client).Client_VoiceStateUpdated;
+      Client.ComponentInteractionCreated += Client_ComponentInteractionCreated;
+      Client.MessageCreated += async (DiscordClient client, MessageCreateEventArgs e) =>
+      {
+        if (e.Message.Content.StartsWith($"<@{client.CurrentUser.Id}>") ||
+            e.Message.Content.StartsWith($"<!@{client.CurrentUser.Id}>")
+            )
+          await e.Message.RespondAsync("'- se quise ve meus comando usa k!help 👍");
 
-            CommandsNextConfiguration commandsConfig = new()
-            {
-                StringPrefixes = new string[] { "k!", "kityme" },
-                EnableMentionPrefix = true,
-                EnableDms = false
-            };
+        return;
+      };
 
-            Commands = Client.UseCommandsNext(commandsConfig);
+      CommandsNextConfiguration commandsConfig = new()
+      {
+        StringPrefixes = new string[] { "ku!", "kityme" },
+        EnableMentionPrefix = true,
+        EnableDms = false
+      };
 
-            /// Commands.SetHelpFormatter<KitymeHelpFormatter>();
+      Commands = Client.UseCommandsNext(commandsConfig);
 
-                Commands.RegisterCommands<ModCommands>();
-                Commands.RegisterCommands<FunCommands>();
-                Commands.RegisterCommands<InfoCommands>();
-                Commands.RegisterCommands<DevCommands>();
-                Commands.RegisterCommands<RPCommands>();
-                Commands.RegisterCommands<MusicCommands>();
-                Commands.RegisterCommands<EconomyCommands>();
+      /// Commands.SetHelpFormatter<KitymeHelpFormatter>();
 
-            SlashCommands = Client.UseSlashCommands();
+      Commands.RegisterCommands<ModCommands>();
+      Commands.RegisterCommands<FunCommands>();
+      Commands.RegisterCommands<InfoCommands>();
+      Commands.RegisterCommands<DevCommands>();
+      Commands.RegisterCommands<RPCommands>();
+      Commands.RegisterCommands<MusicCommands>();
+      Commands.RegisterCommands<EconomyCommands>();
 
-                SlashCommands.RegisterCommands<MusicSlashCommands>();
-                SlashCommands.RegisterCommands<TestingSlashCommands>(720709045119352862); // Guild para testes de slash commands
+      SlashCommands = Client.UseSlashCommands();
+
+      SlashCommands.RegisterCommands<MusicSlashCommands>();
+      SlashCommands.RegisterCommands<TestingSlashCommands>(720709045119352862); // Guild para testes de slash commands
 
 
-            ConnectionEndpoint[] endpoint = new[]
-            {
+      ConnectionEndpoint[] endpoint = new[]
+      {
                 new ConnectionEndpoint()
                 {
                     Hostname = botConfig.LavalinkHost,
@@ -115,8 +113,8 @@ namespace Kityme
                 }
             };
 
-            LavalinkConfiguration[] lavalinkConfig = new[]
-            {
+      LavalinkConfiguration[] lavalinkConfig = new[]
+      {
                 new LavalinkConfiguration()
                 {
                     Password = botConfig.LavalinkPassword,
@@ -134,105 +132,105 @@ namespace Kityme
             };
 
 
-            Lavalink = Client.UseLavalink();
-            Client.UseInteractivity(new()
-            {
-                Timeout = TimeSpan.FromSeconds(30)
-            });
+      Lavalink = Client.UseLavalink();
+      Client.UseInteractivity(new()
+      {
+        Timeout = TimeSpan.FromSeconds(30)
+      });
 
-            DBManager.Connect(botConfig.MongoUrl);
+      DBManager.Connect(botConfig.MongoUrl);
 
-            await Client.ConnectAsync();
-            foreach (LavalinkConfiguration lavalinkConfiguration in lavalinkConfig)
-            {
-                try
-                {
-                    await Lavalink.ConnectAsync(lavalinkConfiguration);
-                }
-                catch (Exception)
-                {
-                    Console.WriteLine("Não foi possivel conectar ao lavalink");
-                }
-            }
-
-            Lavalink.NodeDisconnected += Lavalink_NodeDisconnected;
-
-            await Task.Delay(-1);
-        }
-
-        private async Task Lavalink_NodeDisconnected(LavalinkNodeConnection sender, NodeDisconnectedEventArgs e)
+      await Client.ConnectAsync();
+      foreach (LavalinkConfiguration lavalinkConfiguration in lavalinkConfig)
+      {
+        try
         {
-            int i = 0;
-            while ((i < 5) && !sender.IsConnected)
-            {
-                Thread.Sleep(5000 * (i + 1));
-                i++;
-                await Lavalink.ConnectAsync(new()
-                {
-                    Password = botConfig.LavalinkPassword,
-                    RestEndpoint = sender.NodeEndpoint,
-                    SocketEndpoint = sender.NodeEndpoint,
-                    SocketAutoReconnect = true
-                });
-            }
+          await Lavalink.ConnectAsync(lavalinkConfiguration);
         }
-
-        private async Task Client_ComponentInteractionCreated(DiscordClient sender,
-            ComponentInteractionCreateEventArgs e)
+        catch (Exception)
         {
-            if (!MusicManagers._managers.ContainsKey(e.Guild.Id))
-                return;
-
-            GuildMusicManager manager = MusicManagers._managers[e.Guild.Id];
-            if (!manager.CanChangeQueue(e.Guild.Members.GetValueOrDefault(e.User.Id))) return;
-            switch (e.Id)
-            {
-                case "m_skip":
-                    await MusicManagers._managers[e.Guild.Id].Skip(-1);//await e.Interaction.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new())
-                    await e.Interaction.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder().WithContent($"pulado! *(pedido por {e.User.Username})*"));
-                    break;
-
-                case "m_skip_previous":
-                    await MusicManagers._managers[e.Guild.Id].Skip(MusicManagers._managers[e.Guild.Id].ActualIndex - 1);
-                    await e.Interaction.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder().WithContent($"pulado soq pra tras! *(pedido por {e.User.Username})*"));
-                    break;
-
-                case "m_loop":
-                    manager._loopEnabled = !manager._loopEnabled;
-                    string loop_pmsg = manager._loopEnabled ? "ativado" : "desativado";
-                    await e.Interaction.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder().WithContent($"loop {loop_pmsg} *(pedido por {e.User.Username})*"));
-                    break;
-
-                case "m_shuffle":
-                    manager._shuffleEnabled = !manager._shuffleEnabled;
-                    string shuffle_msg = manager._shuffleEnabled ? "ativado" : "desativado";
-                    //await e.Channel.SendMessageAsync($"embaralhamento {shuffle_msg} *(pedido por {e.User.Username})*");
-                    await e.Interaction.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder().WithContent($"embaralhamento {shuffle_msg} *(pedido por {e.User.Username})*"));
-                    break;
-
-                case "m_stop":
-                    await manager.LastMessage?.DeleteAsync();
-                    await manager.Connection.DisconnectAsync();
-                    manager.RemoveThis(e.Guild.Id);
-                    //await e.Channel.SendMessageAsync($"sai do canal! *(pedido por {e.User.Username})*");
-                    await e.Interaction.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder().WithContent($"sai do canal! * (pedido por { e.User.Username})*"));
-                    break;
-
-                case "sm_select":
-                    StringBuilder stringBuilder = new StringBuilder();
-                    foreach(string value in e.Values)
-                    {
-                        // LavalinkTrack track = JsonConvert.DeserializeObject<LavalinkTrack>(jsonTrack);
-                        var res = await manager.Node.Rest.GetTracksAsync(value);
-                        LavalinkTrack track = res.Tracks.First();
-                        manager._queue.Add(track);
-                        stringBuilder.Append($" {track.Title} |");
-                    }
-                    await e.Interaction.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder().WithContent($"{stringBuilder} adicionado na fila *(pedido por {e.User.Username})*"));
-                    break;
-            }
-
-            return;
+          Console.WriteLine("Não foi possivel conectar ao lavalink");
         }
+      }
+
+      Lavalink.NodeDisconnected += Lavalink_NodeDisconnected;
+
+      await Task.Delay(-1);
     }
+
+    private async Task Lavalink_NodeDisconnected(LavalinkNodeConnection sender, NodeDisconnectedEventArgs e)
+    {
+      int i = 0;
+      while ((i < 5) && !sender.IsConnected)
+      {
+        Thread.Sleep(5000 * (i + 1));
+        i++;
+        await Lavalink.ConnectAsync(new()
+        {
+          Password = botConfig.LavalinkPassword,
+          RestEndpoint = sender.NodeEndpoint,
+          SocketEndpoint = sender.NodeEndpoint,
+          SocketAutoReconnect = true
+        });
+      }
+    }
+
+    private async Task Client_ComponentInteractionCreated(DiscordClient sender,
+        ComponentInteractionCreateEventArgs e)
+    {
+      if (!MusicManagers._managers.ContainsKey(e.Guild.Id))
+        return;
+
+      GuildMusicManager manager = MusicManagers._managers[e.Guild.Id];
+      if (!manager.CanChangeQueue(e.Guild.Members.GetValueOrDefault(e.User.Id))) return;
+      switch (e.Id)
+      {
+        case "m_skip":
+          await MusicManagers._managers[e.Guild.Id].Skip(-1);//await e.Interaction.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new())
+          await e.Interaction.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder().WithContent($"pulado! *(pedido por {e.User.Username})*"));
+          break;
+
+        case "m_skip_previous":
+          await MusicManagers._managers[e.Guild.Id].Skip(MusicManagers._managers[e.Guild.Id].ActualIndex - 1);
+          await e.Interaction.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder().WithContent($"pulado soq pra tras! *(pedido por {e.User.Username})*"));
+          break;
+
+        case "m_loop":
+          manager._loopEnabled = !manager._loopEnabled;
+          string loop_pmsg = manager._loopEnabled ? "ativado" : "desativado";
+          await e.Interaction.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder().WithContent($"loop {loop_pmsg} *(pedido por {e.User.Username})*"));
+          break;
+
+        case "m_shuffle":
+          manager._shuffleEnabled = !manager._shuffleEnabled;
+          string shuffle_msg = manager._shuffleEnabled ? "ativado" : "desativado";
+          //await e.Channel.SendMessageAsync($"embaralhamento {shuffle_msg} *(pedido por {e.User.Username})*");
+          await e.Interaction.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder().WithContent($"embaralhamento {shuffle_msg} *(pedido por {e.User.Username})*"));
+          break;
+
+        case "m_stop":
+          await manager.LastMessage?.DeleteAsync();
+          await manager.Connection.DisconnectAsync();
+          manager.RemoveThis(e.Guild.Id);
+          //await e.Channel.SendMessageAsync($"sai do canal! *(pedido por {e.User.Username})*");
+          await e.Interaction.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder().WithContent($"sai do canal! * (pedido por { e.User.Username})*"));
+          break;
+
+        case "sm_select":
+          StringBuilder stringBuilder = new StringBuilder();
+          foreach (string value in e.Values)
+          {
+            // LavalinkTrack track = JsonConvert.DeserializeObject<LavalinkTrack>(jsonTrack);
+            var res = await manager.Node.Rest.GetTracksAsync(value);
+            LavalinkTrack track = res.Tracks.First();
+            manager._queue.Add(track);
+            stringBuilder.Append($" {track.Title} |");
+          }
+          await e.Interaction.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder().WithContent($"{stringBuilder} adicionado na fila *(pedido por {e.User.Username})*"));
+          break;
+      }
+
+      return;
+    }
+  }
 }
