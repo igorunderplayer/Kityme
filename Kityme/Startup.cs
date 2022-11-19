a215e1ccdf7d82008d546c8945ddc8f755fee4b3
igorunderplayer <igorschug6@gmail.com>
2022-11-19 14:03:27 -0300

    🤡

--- Kityme/Startup.cs
+++ Kityme/Startup.cs
@@ -95,7 +95,7 @@ namespace Kityme
             var endpoint = new ConnectionEndpoint()
             {
                 Hostname = "127.0.0.1",
-                Port = Enviroment.GetEnvironmentVariable("PORT")
+                Port = Environment.GetEnvironmentVariable("PORT")
             };
 
             var lavalinkConfig = new LavalinkConfiguration()
